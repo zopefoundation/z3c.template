967980b7a28e8a848d04f4537d3e2c813fc8b6c4
Dan Korostelev <nadako@gmail.com>
2009-02-26 14:33:18 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.template',
-    version='1.1.0',
+    version='1.2.1dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A package implementing advanced Page Template patterns.",

41d716a99eb23a8bfb5895c181860d06f13c322f
Michael Howitz <mh@gocept.com>
2009-08-22 11:53:19 +0000

    Back to development: 1.2.2dev

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.template',
-    version = '1.2.1',
+    version = '1.2.2dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A package implementing advanced Page Template patterns.",

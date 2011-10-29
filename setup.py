391efc114aa1a300f4fab4360521894aebcedfce
Roger Ineichen <roger@projekt01.ch>
2011-10-29 21:23:12 +0000

    back to dev

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.template',
-    version = '1.4.0',
+    version = '1.4.1dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A package implementing advanced Page Template patterns.",

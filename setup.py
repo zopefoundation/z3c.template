0b1649b50de3d6f5c78d4beef1ec3fba9aee43b9
Adam Groszer <agroszer@gmail.com>
2015-11-09 15:18:09 +0100

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup (
     name='z3c.template',
-    version='2.0.0',
+    version='2.0.1.dev0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "A package implementing advanced Page Template patterns.",

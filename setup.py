dfe9b7a6f3e4fb1e9e4138bae70e52c6b7b60012
Michael Howitz <icemac@gmx.net>
2025-04-14 08:33:33 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ TESTS_REQUIRE = CHAMELEON_REQUIRES + [
 
 setup(
     name='z3c.template',
-    version='5.0',
+    version='5.1.dev0',
     author="Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.dev",
     description="A package implementing advanced Page Template patterns.",
